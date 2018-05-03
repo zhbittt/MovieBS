5bf59e979f8b5bb2a6e2aaa2fbe0d86fda235b12
罗敏文 <shixi_minwen1@staff.weibo.com>
2018-05-04 01:28:28 +0800

    后台管理

--- film/models.py
+++ film/models.py
@@ -7,7 +7,7 @@ class UserInfo(models.Model):
     '''
     username = models.CharField(verbose_name="用户名",max_length=32)
     password = models.CharField(verbose_name="密码",max_length=64)
-    head_img = models.FileField(verbose_name='头像', upload_to='upload', default="/media/upload/default.jpg")
+    head_img = models.FileField(verbose_name='头像', upload_to='upload', default="/media/upload/default.jpg",null=True,blank=True)
     phone = models.CharField(verbose_name="电话",max_length=11,blank=True,null=True)
 
     def __str__(self):
@@ -24,7 +24,9 @@ class MovieDetail(models.Model):
     """电影详情"""
     name = models.CharField(verbose_name="电影名",max_length=32)
     period = models.CharField(verbose_name="电影时长",max_length=32)
-    img = models.CharField(verbose_name="电影图片",max_length=32)
+    # img = models.CharField(verbose_name="电影图片",max_length=32)
+    img = models.FileField(verbose_name='电影图片', upload_to='upload', default="/media/upload/default_film.jpg" ,null=True,blank=True)
+
     info = models.TextField(verbose_name="电影简介",max_length=255,null=True,blank=True)
     plot = models.TextField(verbose_name="电影剧情",max_length=1024,null=True,blank=True)
     company = models.CharField(verbose_name="发行公司",max_length=32,null=True,blank=True)
@@ -46,7 +48,8 @@ class Worker(models.Model):
 
     act_chocies = ((1,"导演"),(2,"编剧"),(3,"演员"))
     Actor = models.IntegerField(verbose_name="职责",choices=act_chocies)
-    role = models.CharField(verbose_name="扮演角色",max_length=32,blank=True,null=True)
+    # role = models.CharField(verbose_name="扮演角色",max_length=32,blank=True,null=True)
+    role = models.FileField(verbose_name='扮演角色', upload_to='upload', default="/media/upload/default_film.jpg",null=True,blank=True)
     act_img = models.CharField(verbose_name="演员图片",max_length=64,blank=True,null=True)
     role_timg = models.CharField(verbose_name="角色图片",max_length=64,blank=True,null=True)
     movie = models.ForeignKey(verbose_name="关联电影",to='MovieDetail',on_delete=models.CASCADE)
@@ -58,8 +61,8 @@ class Comment(models.Model):
     """电影评论表"""
 
     content = models.TextField(verbose_name="评论内容",max_length=255,blank=True,null=True)
-    up_count = models.IntegerField(default=0)
-    down_count = models.IntegerField(default=0)
+    up_count = models.IntegerField(default=0,verbose_name="赞")
+    down_count = models.IntegerField(default=0,verbose_name="踩")
     create_data = models.DateTimeField(verbose_name="评论时间",auto_now_add=True)
 
     parent_comment = models.ForeignKey('self', blank=True, null=True, verbose_name='父级评论',on_delete=models.CASCADE)
@@ -104,7 +107,7 @@ class MovieField(models.Model):
     play_date = models.DateField(verbose_name="播放日期")
     ballot = models.IntegerField(verbose_name="票数")
 
-    movie = models.ForeignKey(verbose_name="电影",to="MovieDetail",on_delete=models.CASCADE)
+    movie = models.ForeignKey(verbose_name="影片",to="MovieDetail",on_delete=models.CASCADE)
     cinema = models.ForeignKey(verbose_name="电影院",to="Cinema",on_delete=models.CASCADE)
 
     def __str__(self):
@@ -121,11 +124,11 @@ class PlayRoom(models.Model):
 class Order(models.Model):
     """订单"""
     payment_type_choices = ((0,"微信"),(1, '支付宝'))
-    payment_type = models.SmallIntegerField(choices=payment_type_choices)
+    payment_type = models.SmallIntegerField(choices=payment_type_choices,verbose_name="支付方式")
     payment_number = models.CharField(max_length=128, verbose_name="支付第3方订单号", null=True, blank=True)
     order_number = models.CharField(max_length=128, verbose_name="订单号", unique=True)  # 考虑到订单合并支付的问题
 
-    user = models.ForeignKey(to="UserInfo",on_delete=models.CASCADE)
+    user = models.ForeignKey(to="UserInfo",on_delete=models.CASCADE,verbose_name="支付人")
     actual_amount = models.FloatField(verbose_name="实付金额")
 
     status_choices = ((0, '交易成功'), (1, '待支付'), (2, '退费申请中'), (3, '已退费'), (4, '主动取消'), (5, '超时取消'))
@@ -139,14 +142,14 @@ class Order(models.Model):
 
 class OrderDetail(models.Model):
     """订单详情"""
-    order = models.ForeignKey("Order",on_delete=models.CASCADE)
-    moviefield = models.ForeignKey(to="MovieField",on_delete=models.CASCADE)
+    order = models.ForeignKey("Order",on_delete=models.CASCADE,verbose_name="关联订单")
+    moviefield = models.ForeignKey(to="MovieField",on_delete=models.CASCADE,verbose_name="影片场次")
     original_price = models.FloatField("电影票原价")
     seat_row = models.IntegerField(verbose_name="行")
     seat_col = models.IntegerField(verbose_name="列")
     content = models.CharField(max_length=255, blank=True, null=True)  # ？
-    valid_period_display = models.CharField("有效期显示", max_length=32)  # 在订单页显示
-    valid_period = models.PositiveIntegerField("有效期(days)")  # 课程有效期
+    valid_period_display = models.CharField("有效期显示", max_length=32,null=True,blank=True)  # 在订单页显示
+    valid_period = models.PositiveIntegerField("有效期(days)",null=True,blank=True)  # 课程有效期
 
     def __str__(self):
         return self.moviefield.movie.name
--- film/stark.py
+++ film/stark.py
@@ -1,6 +1,6 @@
 from stark.service import v1
 from film import models
-
+import datetime
 # show_edit_link = False
 # edit_link = [点击修改字段]
 
@@ -31,17 +31,15 @@ from film import models
 #     if is_header:
 #         return "班级人数"
 #     a = models.Student.objects.filter(class_list=obj.pk).count()
-#
 #     return a
-class UserInfoConfig(v1.StarkConfig):
 
+class UserInfoConfig(v1.StarkConfig):
     list_display = ["username","password","phone"]
     edit_link = ["username"]
 
 v1.site.registry(models.UserInfo,UserInfoConfig)
 
 class MovieTypeConfig(v1.StarkConfig):
-
     list_display = ["name"]
     edit_link = ["name"]
 
@@ -55,7 +53,168 @@ class MovieDetailConfig(v1.StarkConfig):
         for item in obj.mtype.all().values_list("name"):
             mtypes.append(item[0])
         return ",".join(mtypes)
-    list_display = ["name","period","company","score","comment_num","like_num","release",mtype]
+
+    def release(self, obj=None, is_header=None):
+        if is_header:
+            return "上映时间"
+        return datetime.datetime.strftime(obj.release,"%Y-%m-%d")
+
+    list_display = ["name","period","company","score","comment_num","like_num",release,mtype]
     edit_link = ["name"]
 
 v1.site.registry(models.MovieDetail,MovieDetailConfig)
+
+
+class WorkerConfig(v1.StarkConfig):
+
+    def Actor(self, obj=None, is_header=None):
+        if is_header:
+            return "职责"
+        return obj.get_Actor_display()
+
+    def movie(self, obj=None, is_header=None):
+        if is_header:
+            return "关联电影"
+        return obj.movie.name
+
+    list_display = ["name",Actor,"role",movie]
+    edit_link = ["name"]
+
+v1.site.registry(models.Worker,WorkerConfig)
+
+class CommentConfig(v1.StarkConfig):
+
+    def parent_comment(self, obj=None, is_header=None):
+        if is_header:
+            return "父级评论"
+        if obj.parent_comment:
+            return obj.parent_comment.content
+        return "无"
+
+    def user(self, obj=None, is_header=None):
+        if is_header:
+            return "评论者"
+        return obj.user.username
+
+    def movie(self, obj=None, is_header=None):
+        if is_header:
+            return "评论电影"
+        return obj.movie.name
+
+    def create_data(self, obj=None, is_header=None):
+        if is_header:
+            return "评论时间"
+        return datetime.datetime.strftime(obj.create_data,"%Y-%m-%d %H:%M")
+
+    list_display = [user,movie,"content",parent_comment,"up_count","down_count",create_data]
+    edit_link = [user]
+    list_acions = [v1.StarkConfig.multi_del]
+    show_actions = True
+v1.site.registry(models.Comment,CommentConfig)
+
+class CinemaConfig(v1.StarkConfig):
+
+    list_display = ["title","addr"]
+    edit_link = ["title"]
+
+v1.site.registry(models.Cinema,CinemaConfig)
+
+
+class MovieFieldConfig(v1.StarkConfig):
+
+    def movie(self, obj=None, is_header=None):
+        if is_header:
+            return "影片"
+        return obj.movie.name
+
+    def cinema(self, obj=None, is_header=None):
+        if is_header:
+            return "影片"
+        return obj.cinema.title
+
+    def play_date(self, obj=None, is_header=None):
+        if is_header:
+            return "播放日期"
+        return datetime.datetime.strftime(obj.play_date,"%Y-%m-%d")
+
+    list_display = ["start_time", "price",play_date,"ballot",movie,cinema]
+    edit_link = ["start_time"]
+
+
+v1.site.registry(models.MovieField, MovieFieldConfig)
+
+class PlayRoomConfig(v1.StarkConfig):
+
+    def cinema(self, obj=None, is_header=None):
+        if is_header:
+            return "关联电影院"
+        return obj.cinema.title
+
+    list_display = ["caption",cinema]
+    edit_link = ["caption"]
+v1.site.registry(models.PlayRoom, PlayRoomConfig)
+
+class OrderConfig(v1.StarkConfig):
+
+    def payment_type(self, obj=None, is_header=None):
+        if is_header:
+            return "支付方式"
+        return obj.get_payment_type_display()
+
+    def user(self, obj=None, is_header=None):
+        if is_header:
+            return "支付人"
+        return obj.user.username
+
+    def status(self, obj=None, is_header=None):
+        if is_header:
+            return "状态"
+        return obj.get_status_display()
+
+    def date(self, obj=None, is_header=None):
+        if is_header:
+            return "订单生成时间"
+        return datetime.datetime.strftime(obj.date,"%Y-%m-%d %H:%M")
+
+    def pay_time(self, obj=None, is_header=None):
+        if is_header:
+            return "付款时间"
+        if obj.pay_time:
+            return datetime.datetime.strftime(obj.pay_time,"%Y-%m-%d %H:%M")
+        return "无"
+
+    def cancel_time(self, obj=None, is_header=None):
+        if is_header:
+            return "订单取消时间"
+        if obj.pay_time:
+            return datetime.datetime.strftime(obj.cancel_time,"%Y-%m-%d %H:%M")
+        return "无"
+
+    list_display = [user,payment_type,"actual_amount","status","payment_number","order_number",date,pay_time,cancel_time]
+    edit_link = [user]
+v1.site.registry(models.Order, OrderConfig)
+
+class OrderDetailConfig(v1.StarkConfig):
+
+    def order(self, obj=None, is_header=None):
+        if is_header:
+            return "关联订单"
+        return obj.order.order_number
+
+    def moviefield(self, obj=None, is_header=None):
+        if is_header:
+            return "影片场次"
+        return "%s-%s"%(obj.moviefield.play_date,obj.moviefield.start_time)
+
+    def seat(self, obj=None, is_header=None):
+        if is_header:
+            return "位置"
+        return "%s 行 %s 列"%(obj.seat_row,obj.seat_col)
+
+
+    list_display = [order,moviefield,"original_price",seat]
+    edit_link = [order]
+
+
+v1.site.registry(models.OrderDetail, OrderDetailConfig)
+
